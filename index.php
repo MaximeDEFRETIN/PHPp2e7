5f2a42ecdce8c68db0035695421875d88d5c5231
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-12 21:01:16 +0100

    Exercice 7

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <title>Partie 2 : Exercice 7</title>
+</head>
+<body>
+  <?php
+  $variable = true;
+
+  if ($variable == false) {
+    echo 'c\'est pas bon !!!';
+  }
+  else {
+    'c\'est ok !!';
+  }
+  ?>
+</body>
+</html>
